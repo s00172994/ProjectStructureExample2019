e26d3b689aabc5a23ebb23fd07ddfd8689bf2a68
ITSPaul <powell.paul@itsligo.ie>
2019-03-10 20:01:13 +0000

    Add project files.

---
+++ ProjectStructureExample2019/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ProjectStructureExample2019.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ProjectStructureExample2019/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjectStructureExample2019.WebApiApplication" Language="C#" %>
