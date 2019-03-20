43157c7adcc5d356b446caceb62b53221c34138e
ITSPaul <powell.paul@itsligo.ie>
2019-03-20 16:54:13 +0000

    Including Generic based Web API Client Project for further development of Console Client

---
+++ CLWebAPIClient/GenericWebAPICalls.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Formatting;
+//using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace WebAPIAuthenticationClient
+{
+    public enum AUTHSTATUS { NONE,OK,INVALID,FAILED }
+
+
+    public static class ClientAuthentication
+    {
+        static public string baseWebAddress;
+        static public string AuthToken = "";
+        static public AUTHSTATUS AuthStatus = AUTHSTATUS.NONE;
+        static public string IgdbUserToken = "PUT YOUR EXTERNAL WEB API TOKEN HERE";
+        static public List<T> getList<T>(string endpoint)
+        // Endpoint api/GameScores/getTops/Count/
+        {
+            using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    var response = client.GetAsync(baseWebAddress + endpoint ).Result;
+                    var resultContent = response.Content.ReadAsAsync<List<T>>(
+                        new[] { new JsonMediaTypeFormatter() }).Result;
+                    return resultContent;
+                }
+            }
+
+        static public dynamic getExtGame(int gameID)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("user-key", IgdbUserToken);
+                var response = client.GetAsync("https://api-endpoint.igdb.com/games/" + gameID.ToString()).Result;
+                var resultContent = response.Content.ReadAsAsync<JToken>(
+                    new[] { new JsonMediaTypeFormatter() }).Result;
+                var jname = resultContent.Children()["name"].Values<string>().FirstOrDefault();
+                var jsummary = resultContent.Children()["summary"].Values<string>().FirstOrDefault();
+                // url is nested in cover object
+                var jcover = resultContent.Children()["cover"]["url"].Values<string>().FirstOrDefault();
+                var eobj = 
+                                        new 
+                                        {
+                                            Name = jname,
+                                            Summary = jsummary,
+                                            Cover = jcover
+                                        };
+                return eobj;
+            }
+        }
+
+        // Endpoint "api/GameScores/playerInfo"
+        static public T getItem<T>(string EndPoint)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",AuthToken);
+                var response = client.GetAsync(baseWebAddress + EndPoint).Result;
+                var resultContent = response.Content.ReadAsAsync<T>(
+                    new[] { new JsonMediaTypeFormatter() }).Result;
+                return resultContent;
+            }
+        }
+
+        // Post a score for the current player  
+
+        // Endpoint = "api/GameScores/postScore"
+        static public bool PostItem<T>(T g, string Endpoint)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = client.PostAsJsonAsync(baseWebAddress + Endpoint,g).Result;
+                if(response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Response Object is " + response.Content.ReadAsAsync<T>(
+                        new[] { new JsonMediaTypeFormatter() }).Result.ToString());
+                    return true;
+                }
+                return false;
+            }
+
+        }
+    static public bool login(string username, string password)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var content = new FormUrlEncodedContent(new[]
+                    {
+                        new KeyValuePair<string, string>("grant_type", "password"),
+                        new KeyValuePair<string, string>("username", username),
+                        new KeyValuePair<string, string>("password", password),
+                    });
+                var result = client.PostAsync(baseWebAddress + "Token", content).Result;
+                try
+                {
+                    var resultContent = result.Content.ReadAsAsync<Token>(
+                        new[] { new JsonMediaTypeFormatter() }
+                        ).Result;
+                    string ServerError = string.Empty;
+                    if (!(String.IsNullOrEmpty(resultContent.AccessToken)))
+                    {
+                        Console.WriteLine(resultContent.AccessToken);
+                        AuthToken = resultContent.AccessToken;
+                        AuthStatus = AUTHSTATUS.OK;
+                        return true;
+                    }
+                    else
+                    {
+                        AuthToken = "Invalid Login" ;
+                        AuthStatus = AUTHSTATUS.INVALID;
+                        Console.WriteLine("Invalid credentials");
+                        return false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    AuthStatus = AUTHSTATUS.FAILED;
+                    AuthToken = "Server Error -> " + ex.Message;
+                    Console.WriteLine(ex.Message);
+                    return false;
+                }
+
+            }
+        }
+
+        
+
+}
+}
